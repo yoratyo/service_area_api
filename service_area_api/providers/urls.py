1898f224fd813fa24152e6442ea4abc32d0b1bf9
Yora Radityohutomo <yoratyo@gmail.com>
2019-07-21 21:11:32 +0700

    Add search area function

--- service_area_api/providers/urls.py
+++ service_area_api/providers/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('providers', views.ProviderView)
-router.register('service_area', views.ServiceAreaView)
+router.register('service-area', views.ServiceAreaView)
 
 urlpatterns = [
     path('', include(router.urls)),
--- service_area_api/providers/views.py
+++ service_area_api/providers/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import  viewsets
 from .models import Provider, ServiceArea
 from .serializers import ProviderSerializer, ServiceAreaSerializer
+from django.contrib.gis.geos import GEOSGeometry
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class ProviderView(viewsets.ModelViewSet):
@@ -12,3 +15,17 @@ class ProviderView(viewsets.ModelViewSet):
 class ServiceAreaView(viewsets.ModelViewSet):
     queryset = ServiceArea.objects.all()
     serializer_class = ServiceAreaSerializer
+
+    @action(methods=['get'], detail=False)
+    def search(self, request):
+        try:
+            longitude = request.query_params.get('longitude')
+            latitude = request.query_params.get('latitude')
+
+            pnt = GEOSGeometry('POINT({} {})'.format(longitude, latitude), srid=4326)
+            result = ServiceArea.objects.filter(polygon__contains=pnt)
+            serializer = self.get_serializer(result, many=True)
+
+            return Response(serializer.data)
+        except:
+            return Response({"Error": "Make sure you set longitude & latitude correctly"})
